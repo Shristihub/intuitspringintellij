fb07616db6cd01e856a181c8673a45009f1791b7
shristitechacademy <sripriyamathan@gmail.com>
2025-05-20 12:53:07 +0530

    book

--- spring-bookapp-jpa/src/main/java/com/bookapp/SpringBookappJpaApplication.java
+++ spring-bookapp-jpa/src/main/java/com/bookapp/SpringBookappJpaApplication.java
@@ -1,13 +1,22 @@
 package com.bookapp;
 
+import com.bookapp.service.IBookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBookappJpaApplication {
+public class SpringBookappJpaApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBookappJpaApplication.class, args);
     }
 
+    @Autowired
+    private IBookService bookService;
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
